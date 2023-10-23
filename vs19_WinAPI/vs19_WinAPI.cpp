831f20bbd66c5ac558793456573167682f612c14
DESKTOP-4LBHU0L\USER <shlee4508@naver.com>
2023-10-23 12:53:06 +0900

    사각형 그리기

--- vs19_WinAPI/vs19_WinAPI.cpp
+++ vs19_WinAPI/vs19_WinAPI.cpp
@@ -166,6 +166,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
             // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+
+            Rectangle(hdc, 10, 10, 110, 110); //사각형 그리기
             EndPaint(hWnd, &ps);
         }
         break;
