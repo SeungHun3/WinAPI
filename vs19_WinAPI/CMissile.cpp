80991a31e496a088e4a4078dfb51e1f5b5fcae86
DESKTOP-4LBHU0L\USER <shlee4508@naver.com>
2023-10-25 16:18:44 +0900

    삼각함수 Θ 종합

--- vs19_WinAPI/CMissile.cpp
+++ vs19_WinAPI/CMissile.cpp
@@ -4,7 +4,7 @@
 #include "CTimeMgr.h"
 
 CMissile::CMissile()
-	: m_fTheta(0.f) // 0도
+	: m_fTheta(PI) // m_fTheta : 0 ~ 1 = 0도 ~ 180도
 {
 }
 
@@ -15,7 +15,7 @@ CMissile::~CMissile()
 void CMissile::update()
 {
 	Vec2 vPos = GetPos();
-	// 대각선 이동시 이동량 1 맞춰주기 => 1라디안
+	// 대각선 이동시 이동량 1 맞춰주기 => x, y 비율계산
 	vPos.x += 600.f * cosf(m_fTheta) * fDT;
 	vPos.y -= 600.f * sinf(m_fTheta) * fDT; 
 
